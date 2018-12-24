b42e27c269c08c46c7197ef47a3aece6f299b62e
nuzcraft <nuzcraft@gmail.com>
2018-12-24 11:45:16 -0600

    finishing 8.2
    ending 8.2 on strings

--- README.md
+++ README.md
@@ -22,3 +22,4 @@ Currently, the repo includes the following:
 * match_control_flow - https://doc.rust-lang.org/book/2018-edition/ch06-02-match.html
 * communicator - https://doc.rust-lang.org/book/2018-edition/ch07-00-modules.html
 * vectors - https://doc.rust-lang.org/book/ch08-01-vectors.html
+* strings - https://doc.rust-lang.org/book/ch08-02-strings.html
--- strings/src/main.rs
+++ strings/src/main.rs
@@ -42,4 +42,108 @@ fn main() {
     let hello = String::from("Olá");
     let hello = String::from("Здравствуйте");
     let hello = String::from("Hola");
+
+    // UPDATING A STRING
+    // String can grow in size and contents can change, similar to vectors. you can use the (+)
+    // operator or format! macro to concatenate String values
+    // APPENDING A String WITH push_str AND push
+    // push_str can be used to grow a string by appending a string slice
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    // now, s=foobar
+    // push_str takes a string slice bc we don't necessarily want to take ownership of the parameter
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {}", s2);
+    // it would be unfortunate if we weren't able to use s2 after appending its contents to s1
+    // if push_str too ownership of s2, we wouldn't be able to print it's value at the end
+
+    // the push method takes a single character as a parameter and adds it to the String
+    let mut s = String::from("lo");
+    s.push('l');
+
+    // Concatenation with the + operator or the format! macro
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // not s1 has been moved here anc can no longer be used
+    // the string s3 will contain Hello, world!. The reason s1 is no longer valid after the addition
+    // and the reason we used a reference to s2 has to do with the signature of the method that gets
+    // called when we use the + operator. The + operator uses the add method, whos signature looks
+    // something like this:
+    // fn add(self, s: &str) -> String {}
+    // this is weird. Essentially, + requires a String (i.e. s1) be fully passed in as 'self'. The
+    // function takes ownership, appends a reference to a string slice (&s2), then passes ownership back
+    // after this, s1 is unusable, s2 and s3 are fine. This is more efficient than copying (there's also
+    // some deref coercion used to get String into a string slice)
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = s1 + "-" + &s2 + "-" + &s3;
+    // concatenation with + can get unwieldy
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    // the fromat! macro can make short work of this. This is formatted similar to println!. Additionally,
+    // it doesn't take ownership of any of the parameters, so s1 is still intact.
+
+    // INDEXING INTO STRINGS
+    // in other programming languages, you can access individual characters of a string via an index. Not
+    // so in Rust; you'll get an error. Rust strings don't support indexing.
+
+    // Internal Representation
+    // String is a wrapper over Vec<u8>
+    let len = String::from("Hola").len();
+    // in this case, len=4, meaning the vector storing the string "Hola" is 4 bytes long
+    let len = String::from("Здравствуйте").len();
+    // in this case the answer is 24 (not 12) because each Unicode scalar takes 2 bytes of
+    // storage instead of 1. Therefore, an index of the string's bytes will not always
+    // correlate to a valid Unicode scalar value
+    // example:
+    // let hello = "Здравствуйте";
+    // let answer = &hello[0];
+    // What should answer be? when encoded in UTF-8, the first byte of З(Ze) is 208 and the second is 151, so 
+    // answer should be 208, but 208 is not a valid character on its own, nor would it actually want to be returned
+    // by the user. To avoid this, rust won't compile the code at all
+
+    // Bytes and Scalar Values and Grapheme Cluster! Oh My!
+    // another point about UTF-8 is that there are actually three relevant ways to look at strings from Rust's perspective:
+    // as bytes, scalar values, and grapheme clusters (the closest to 'letters')
+    // looking at the Hini word नमस्ते written in Devanagari script, it is stored as a vector of u8 values that look like: 
+    // [224, 164, 168, 224, 164, 174, 224, 164, 184, 224, 165, 141, 224, 164, 164, 224, 165, 135]
+    // That is 18 bytes and is how computers ultimately store the data. 
+    // We can look at them as Unicode scalar values (which are what Rust's char type is)
+    // ['न', 'म', 'स', '्', 'त', 'े']
+    // There are six char values here, but the 4th and 5th are not letters, they are diacritics that don't make sense on their
+    // own.
+    // Finally, we can look at them as grapheme clusters
+    // ["न", "म", "स्", "ते"]
+    // this is what we'd call the four letters that make up the hindi word
+
+    // Slicing Strings
+    // indexing into a string is often bad bc it's not clear what the return type of the string indexing operation should be:
+    // byte, character, grapheme cluster, or string slice
+    // Therefore, Rust asks you to be more specific when you need to crate a string slice
+    let hello = "Здравствуйте";
+    let s = &hello[0..4];
+    // s will be and &str that contains the first 4 bytes of the string (Зд)
+    // if the slice isn't a full character (for example &hello[0..1]), it would panic at runtime, and can crash the program
+
+    // Methods for Iterating over Strings
+    // you can perform operations over individual Unicode values with the chars method
+    for c in "नमस्ते".chars() {
+        println!("{}", c); // will return the 6 unicode scalar values
+    }
+    // we can also look at the bytes
+    for b in "नमस्ते".bytes() {
+        println!("{}", b); // will return the 18 bytes
+    }
+    // getting grapheme clusters is complex, and isn't included in the standard library (crates at crates.io are available though)
+
+    // Strings are not so simple
+    // Essentially, developers dealing with this ahead of time makes it less likely something will break later
+
 }
