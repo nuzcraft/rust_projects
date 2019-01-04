a38fb68bbff048ffab1c67b1c102bb124ac5921e
nuzcraft <nuzcraft@gmail.com>
2019-01-04 13:19:12 -0600

    additional error handling
    and the ability to 'exit' at any time

--- chapter_8_exercises/src/exercises/mean_median_mode.rs
+++ chapter_8_exercises/src/exercises/mean_median_mode.rs
@@ -4,94 +4,141 @@ use std::io;
 pub fn main() {
 
     println!("Welcome to Mean, Median, Mode.");
-    println!("Please input an integer.");
+    // create input loop that waits for a valid input
 
-    // ask the user to print an integer
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)
-        .expect("Failed to read line"); // we'll want to switch to a match to handle
+    let mut integer_input: bool = false;
+    let mut int: i32 = 0; // start with 0
+    let mut exit: bool = false;
 
-    let int: i32 = input.trim().parse()
-        .expect("Please type a number!"); // we'll want to switch to a match to handle
-    
-    // create a blank vector
-    let mut v: Vec<i32> = Vec::new();
-    // push the typed integer into the vector
-    v.push(int);
-    println!("The current vector is: {:?}", v);
-
-    // create a loop to let the user add integers if desired
-    loop {
-        // ask for an integer, place it in a new string each time
-        println!("Please input an integer or 'end'.");
-        let mut input2 = String::new();
-        io::stdin().read_line(&mut input2)
+    while integer_input == false { // we only want to continue if an integer is input
+        // ask the user to print an integer
+        println!("Please input an integer.");
+        // take input from the user
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)
             .expect("Failed to read line");
-        match input2.as_str().trim() {
-            "end" => break, // if 'end' is typed, break our of the loop
-            _ => {
-                let int: i32 = input2.trim().parse()
-                    .expect("Please type a number!"); // if an int is typed, add it to the vectors, else crash
-                v.push(int);
-                println!("The current vector is: {:?}", v);
+
+        // if 'exit' is typed, exit set the exit flag to true
+        match input.as_str().trim() {
+            "exit" => {
+                exit = true;
+                integer_input = true;
             },
+            _ => (),
+        };
+        
+        if exit == false {
+            // try to parse the input into an integer
+            match input.trim().parse() {
+                Ok(num) => { // if good, set the int to the number, and set the flag to true
+                    int = num;
+                    integer_input = true;
+                },
+                Err(_) => { // if error, display an error message, and let them try again
+                    println!("You typed: {}. Please type an integer.", input.trim());
+                    continue;
+                },
+            }
         }
     }
-
-    v.sort(); // sort the vector (must be mutable)
-    println!("The vector is {:?}", v);
     
-    // Get the number of elements in the vector; assert that this cannot be 0
-    let len: i32 = v.len() as i32;
-    assert_ne!(v.len(), 0);
+    if exit == false {
+        // create a blank vector
+        let mut v: Vec<i32> = Vec::new();
+        // push the typed integer into the vector
+        v.push(int);
+        println!("The current vector is: {:?}", v);
 
-    // get and print the mean by looping through the vector
-    // at the same time, look for the median
-    let mut sum: i32 = 0;
-    for i in &v {
-        sum += i;
-    }
-    // cast the sum and len as floats for decimal division
-    let mean: f64 = sum as f64 / len as f64;
-    println!("The mean is: {}", mean);
+        // create a loop to let the user add integers if desired
+        loop {
+            // ask for an integer, place it in a new string each time
+            println!("Please input an integer or 'end'.");
+            let mut input2 = String::new();
+            io::stdin().read_line(&mut input2)
+                .expect("Failed to read line");
+            match input2.as_str().trim() {
+                "end" => break, // if 'end' is typed, break our of the loop
+                "exit" => {
+                    exit = true;
+                    break;
+                },
+                _ => {
+                    match input2.trim().parse() {
+                        Ok(num) => { // if good, set the int to the number
+                            int = num;
+                        },
+                        Err(_) => { // if error, display an error message, and let them try again
+                            println!("You typed: {}. Please type an integer.", input2.trim());
+                            continue;
+                        },
+                    }
+                    v.push(int);
+                    println!("The current vector is: {:?}", v);
+                },
+            }
+        }
 
-    // find if there's an even or odd number of of items in the list
-    if is_odd(len) {
-        // if theres an odd number, we can find the middle one and print it
-        let median_index: usize = (len as usize - 1) / 2;
-        println!("The median is: {}", v[median_index]);
+        if exit == false {
+            v.sort(); // sort the vector (must be mutable)
+            println!("The vector is {:?}", v);
+            
+            // Get the number of elements in the vector; assert that this cannot be 0
+            let len: i32 = v.len() as i32;
+            assert_ne!(v.len(), 0);
+
+            // get and print the mean by looping through the vector
+            // at the same time, look for the median
+            let mut sum: i32 = 0;
+            for i in &v {
+                sum += i;
+            }
+            // cast the sum and len as floats for decimal division
+            let mean: f64 = sum as f64 / len as f64;
+            println!("The mean is: {}", mean);
+
+            // find if there's an even or odd number of of items in the list
+            if is_odd(len) {
+                // if theres an odd number, we can find the middle one and print it
+                let median_index: usize = (len as usize - 1) / 2;
+                println!("The median is: {}", v[median_index]);
+            } else {
+                // if there's an even number, we need to find the average of the two middle values
+                let first_median_index: usize = ((len as usize) / 2) - 1;
+                let second_median_index = &first_median_index + 1;
+                let median: f64 = (v[first_median_index] as f64 + v[second_median_index] as f64) / 2 as f64;
+                println!("The median is: {}", median);
+            }
+
+            // create a HashMap for each entry in the vector, counting the number
+            // of times each number is used. this code is pulled from chapter 8 (and is really cool)
+            let mut mode_map = HashMap::new();
+            for i in &v {
+                let count = mode_map.entry(i).or_insert(0);
+                * count += 1;
+            }
+            // loop through the hash map, finding the highest entries, then, create a
+            // new vector with those values
+            let mut highest_value: i32 = 0;
+            for (_key, value) in &mode_map {
+                if value > &highest_value {
+                    highest_value = *value;
+                }
+            }
+            let mut mode_v: Vec<i32> = Vec::new();
+            for (key, value) in &mode_map {
+                if value == &highest_value {
+                    mode_v.push(**key);
+                }
+            }
+            // IMPORTANT: {:?} is the debug mode print, vectors do not have a legitimate print mode
+            mode_v.sort();
+            println!("The mode(s) are: {:?}", mode_v);
+        } else {
+            println!("Exiting Mean, Median, Mode. Goodbye.", )
+        }
     } else {
-        // if there's an even number, we need to find the average of the two middle values
-        let first_median_index: usize = ((len as usize) / 2) - 1;
-        let second_median_index = &first_median_index + 1;
-        let median: f64 = (v[first_median_index] as f64 + v[second_median_index] as f64) / 2 as f64;
-        println!("The median is: {}", median);
+    println!("Exiting Mean, Median, Mode. Goodbye.");
     }
-
-    // create a HashMap for each entry in the vector, counting the number
-    // of times each number is used. this code is pulled from chapter 8 (and is really cool)
-    let mut mode_map = HashMap::new();
-    for i in &v {
-        let count = mode_map.entry(i).or_insert(0);
-        * count += 1;
-    }
-    // loop through the hash map, finding the highest entries, then, create a
-    // new vector with those values
-    let mut highest_value: i32 = 0;
-    for (_key, value) in &mode_map {
-        if value > &highest_value {
-            highest_value = *value;
-        }
-    }
-    let mut mode_v: Vec<i32> = Vec::new();
-    for (key, value) in &mode_map {
-        if value == &highest_value {
-            mode_v.push(**key);
-        }
-    }
-    // IMPORTANT: {:?} is the debug mode print, vectors do not have a legitimate print mode
-    mode_v.sort();
-    println!("The mode(s) are: {:?}", mode_v);
 }
 
 // this function is a simple match on the remainder 
--- chapter_8_exercises/src/main.rs
+++ chapter_8_exercises/src/main.rs
@@ -4,21 +4,34 @@ use std::io;
 mod exercises;
 
 fn main() {
+    let mut exit: bool = false;
+    while exit == false {
+        println!("Please choose an exercise:");
+        println!("Type 'exit' at any time to exit.");
+        println!("a: Mean, Median Mode");
+        println!("b: Pig Latin");
+        println!("c: Text Interface");
 
-    println!("Please choose an exercise:");
-    println!("a: Mean, Median Mode");
-    println!("b: Pig Latin");
-    println!("c: Text Interface");
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice)
+            .expect("Failed to read line");
 
-    let mut choice = String::new();
-    io::stdin().read_line(&mut choice)
-        .expect("Failed to read line");
-
-    // each exercise is it's own module within the exercise module. Each has 
-    // a 'main' function used to do its thing
-    match choice.as_str().trim() { // trim is used to pull off the newline
-        "a" => {exercises::mean_median_mode::main();},
-        "b" => {exercises::pig_latin::main(String::from("hello"));},
-        _ => println!("Please choose one of the given options."),
+        // each exercise is it's own module within the exercise module. Each has 
+        // a 'main' function used to do its thing
+        match choice.as_str().trim() { // trim is used to pull off the newline
+            "a" => {
+                exercises::mean_median_mode::main();
+                println!(""); // print a newline
+                },
+            "b" => {
+                exercises::pig_latin::main(String::from("hello"));
+                println!(""); // print a newline
+                },
+            "exit" => {
+                println!("Goodbye!");
+                exit = true;
+            }
+            _ => (),
+        }
     }
 }
